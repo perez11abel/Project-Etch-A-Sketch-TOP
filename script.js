658b3f4cb86640400bc656f74a4bd8f60a3da084
abel <abel_perez9@hotmail.com>
2022-12-07 17:01:39 -0600

    add functionality to buttons

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+let color = 'black';
+
 document.addEventListener('DOMContentLoaded', () =>{
-    createBoard(32);
-    console.log('hi');
+    createBoard(16);
+    let btn_popup = document.querySelector('#popup');
+    btn_popup.addEventListener('click', () => {
+        let size = getSize();
+        createBoard(size);
+    })
 });
 
 const createBoard = (size) => {
@@ -13,7 +19,38 @@ const createBoard = (size) => {
 
     for(let i = 0 ; i < numDivs; i++) {
         let div = document.createElement('div');
-        div.style.backgroundColor = 'yellow';
+        div.addEventListener('mouseover', colorDiv);
         board.insertAdjacentElement('beforeend', div);
     }
+}
+
+const getSize = () => {
+    let input = prompt('Enter Size of Board.');
+    let message = document.querySelector('#message');
+    if (input === '') {
+        //innerHTML will add text to the paragraph.
+        message.innerHTML = 'Please Provide A Number';
+    } else if ( input < 0 || input > 100) {
+        message.innerHTML = 'Please Provide A Number between 1 & 100';
+    } else {
+        message.innerHTML = 'Lets Sketch';
+        return input;
+    }
+}
+//using arrow functions causes 'this' not to work.
+function colorDiv() {
+    if(color == 'random') {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+    } else {
+        this.style.backgroundColor = 'black';
+    }
+};
+
+const setColor = (colorChoice) => {
+    color = colorChoice;
+}
+
+const resetBoard = () => {
+    let divs = document.querySelectorAll('div');
+    divs.forEach(div => div.style.backgroundColor = 'white');
 }
